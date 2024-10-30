20f8a3ab48927d8fde62106b7d4529ab83d98c98
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-30 17:49:44 +0800

    Corrected the algorithm that well aligns to the objective (specifically the approach to this part/section of the program) of the program. Can evaluate the input/s if it is a digit or not (other characters; alphabets, special characters, etc.).

--- input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
+++ input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
@@ -1,36 +1,19 @@
 
+
 while True:
-    while True:
-        user_input = input("Enter a number (1-50): ")
-        if user_input.isdigit():    
-            user_input = int(user_input)  
-            if 1 <= user_input <= 50:
-                break
-            else:
-                print("Invalid input, must be between 1 and 50.")
-                print("Please try again.")        
-        else:
-            print("Invalid input, must be a digit.")
-            print("Please try again.")
-    
-    while True:
-        another_entry = input("Would you like to submit another number? (Yes/No): ").capitalize()    
-        # if statement evaluating if the response is == to 'No'  
-        if another_entry == "No":
-            # break if returned true
+    user_input = input("Enter a number (1-50): ")
+    if user_input.isdigit():
+        user_input = int(user_input)
+        if user_input > 50:
+            print("Invalid number.")
+            print("Proceeding to: 'presenting the data' (processing...)")
+            break 
+        elif user_input < 1:
+            print("Invalid number.")
+            print("Proceeding to: 'presenting the data' (processing...)")
             break
-            # elif statement evaluating if or else the respond is == to 'Yes'
-        elif another_entry == "Yes":
-            # break if returned true
-            break
-        # else statement; raises the error
-        else: 
-            # print invalid
-            print("Invalid input, 'Yes' or 'No' only.")
-            # notice the user/s to redo
-            print("Please try again.")      
+    else:
+        print("Invalid input, must be a digit.")
+        print("Please try again.")
     
-    # if statement that is looped inside the loop 1; after the first evaluation for 'No' response, this will be the final decider to finished the main loop (loop 1), if this returned false, the main loop will start again; only means that the user/s respond with a 'Yes'
-    if another_entry == "No":
-        # break the main loop (loop 1)
-        break 
+  
