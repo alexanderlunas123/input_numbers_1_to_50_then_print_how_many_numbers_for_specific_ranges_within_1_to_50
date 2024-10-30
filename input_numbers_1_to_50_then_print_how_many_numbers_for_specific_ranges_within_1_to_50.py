44c62c9c770598f3eaa586d727bbe7e792ac420a
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-30 19:17:11 +0800

    Brainstormed for formulating an algorithm that stores each entries to an array and then present it with the condition of: presenting the numberof elements in a specific range. It cost me more than an hour to think for the logic since the previous trial and errors does not quiet fit to how the algorithm should be. Now I will just reintroduce this algorithm for the other range to succesfully finish the task (main program).

--- input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
+++ input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
@@ -1,4 +1,4 @@
-
+entries_for_1_to_10 = []
 
 while True:
     user_input = input("Enter a number (1-50): ")
@@ -12,8 +12,13 @@ while True:
             print("Invalid number.")
             print("Proceeding to: 'presenting the data' (processing...)")
             break
+        else:
+            if 1 <= user_input <= 10:
+                entries_for_1_to_10.append(user_input)             
     else:
         print("Invalid input, must be a digit.")
         print("Please try again.")
-    
-  
+
+
+numbers_of_1_to_10 = len(entries_for_1_to_10)  
+print(numbers_of_1_to_10)     
