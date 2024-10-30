2857a15187403db8e04d64840347b646385d3b8c
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-30 19:51:18 +0800

    Finished the overall algorithm; successfully operates the expected output for the main objective of this program. Then I will do some designing or just making the interface more understandable and contextual for the user/s. I'm not sure though if I will put a comment/pseudocode just to explain this algorithm since the basics are already given to my previous projects with well explanations but I guess it is better to also do here since this is a different program.

--- input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
+++ input_numbers_1_to_50_then_print_how_many_numbers_for_specific_ranges_within_1_to_50.py
@@ -1,4 +1,8 @@
 entries_for_1_to_10 = []
+entries_for_11_to_20 = []
+entries_for_21_to_30 = []
+entries_for_31_to_40 = []
+entries_for_41_to_50 = []
 
 while True:
     user_input = input("Enter a number (1-50): ")
@@ -14,11 +18,28 @@ while True:
             break
         else:
             if 1 <= user_input <= 10:
-                entries_for_1_to_10.append(user_input)             
+                entries_for_1_to_10.append(user_input)
+            elif 11 <= user_input <= 20:
+                entries_for_11_to_20.append(user_input)
+            elif 21 <= user_input <= 30:
+                entries_for_21_to_30.append(user_input)    
+            elif 31 <= user_input <= 40:
+                entries_for_31_to_40.append(user_input)    
+            elif 41 <= user_input <= 50:
+                entries_for_41_to_50.append(user_input)    
     else:
         print("Invalid input, must be a digit.")
         print("Please try again.")
 
+print("Result:")
 
 numbers_of_1_to_10 = len(entries_for_1_to_10)  
-print(numbers_of_1_to_10)     
+numbers_of_11_to_20 = len(entries_for_11_to_20)
+numbers_of_21_to_30 = len(entries_for_21_to_30)
+numbers_of_31_to_40 = len(entries_for_31_to_40)
+numbers_of_41_to_50 = len(entries_for_41_to_50)
+print(f"1-10 = {numbers_of_1_to_10}")     
+print(f"11-20 = {numbers_of_11_to_20}")
+print(f"21-30 = {numbers_of_21_to_30}")
+print(f"31-40 = {numbers_of_31_to_40}")
+print(f"41-50 = {numbers_of_41_to_50}")
